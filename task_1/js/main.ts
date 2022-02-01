c2050e559e267e03c7539e228de75f2fc6fc278d
Wiston Venera Macías <wisvem@hotmail.com>
2022-02-01 20:08:42 +0000

    Task 2

--- task_1/js/main.ts
+++ task_1/js/main.ts
@@ -10,3 +10,11 @@ interface Teacher {
 interface Directors extends Teacher {
     numberOfReports: number;
 }
+
+function printTeacher(firstName: string, lastName: string){
+    return(firstName[0]+". "+lastName);
+}
+
+interface printTeacherFunction {
+    (firstName: string, lastName: string): string;
+}
