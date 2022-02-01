dc53f4c64d728b4afd6600efd3576f6a2dffa91a
Wiston Venera Macías <wisvem@hotmail.com>
2022-02-01 14:51:26 +0000

    Task 2

--- task_1/js/main.ts
+++ task_1/js/main.ts
@@ -2,7 +2,11 @@ interface Teacher {
     readonly firstName: string;
     readonly lastName: string;
     fullTimeEmployee: boolean;
-    yearsOfExperience: number;
+    yearsOfExperience?: number;
     location: string;
     [attributes: string]: any;
 };
+
+interface Directors extends Teacher {
+    numberOfReports: number;
+}
