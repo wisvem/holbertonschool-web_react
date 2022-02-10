ef555ab307b2ff1e6bc89b425eb0b003267c0cf8
Wiston Venera Macías <wisvem@hotmail.com>
2022-02-10 02:30:18 +0000

    Changes to index

--- 0x02-react_intro/task_1/dashboard/src/index.js
+++ 0x02-react_intro/task_1/dashboard/src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Notifications from './Notifications';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <div className='root-notifications'>
+      <Notifications />
+    </div>  
   </React.StrictMode>,
   document.getElementById('root')
 );
